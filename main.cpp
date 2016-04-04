fe18538c84bfe8e6da814f97b30a7b7181b5b85b
Erik Sandgren <erik.sandgren92@gmail.com>
2016-04-04 14:36:31 +0200

    Added funcitonallity to save delay-pdf when simulating

--- main.cpp
+++ main.cpp
@@ -19,7 +19,9 @@
 using namespace std; // Using standard library namespace.
 
 // Declaration of function to evaluate PLR for a certain load, defined further down...
-void evaluate_load(double g, int n, int n_rx,int d[],  double Lambda[],  int q,  int iter_max,  int how_often_to_decode,  bool boundary_effect,  bool first_slot_tx,  int num_packets_to_sim, int num_PL_to_sim, vector<double>* PLR,int index, int maximum_delay);
+void evaluate_load(double g, int n, int n_rx,int d[],  double Lambda[],  int q,  int iter_max,  int how_often_to_decode,  bool boundary_effect,  bool first_slot_tx,  int num_packets_to_sim, int num_PL_to_sim, vector<double> &PLR,int index, int maximum_delay, vector<vector<int>> &delays, bool save_delays);
+
+
 
 int main () {
     // Define all parameters:
@@ -35,7 +37,7 @@ int main () {
     // vector to save packet loss rate results
     vector<double> PLR(g_len); // All elements are initially 0 this way
     
-    num_packets_to_sim =(int)1e6;  // How many sent packets to simulate (at most) for each system-load value
+    num_packets_to_sim =(int)1e5;  // How many sent packets to simulate (at most) for each system-load value
     num_PL_to_sim=num_packets_to_sim/1000; // How many packet losses to simulate (at most) for each system-load value
     
     // Defines the VN-degree distribution
@@ -49,12 +51,19 @@ int main () {
     
     bool boundary_effect=false; // Do you want to consider a system with boundary effect?
     bool save_delays=false;  // Do you want to save the packet-delay pdf?
-    bool first_slot_tx=false; // Do you want to transmit in first slot or use uniform edge distribution?
+    bool first_slot_tx=true; // Do you want to transmit in first slot or use uniform edge distribution?
     
+    vector<vector<int>> delays (0,vector<int>(0,0));
+    if(save_delays)
+    {
+        delays.resize(g_len,vector<int>(maximum_delay,0));
+    }
     // where to save the output and by which name:
     ofstream output_file;
     string filename = "test" + to_string(n) + "n_rx_" + to_string(n_rx)+"max_delay_" + to_string(maximum_delay) +"_BE_"+ to_string(boundary_effect)+"_firstslottx_" +to_string(first_slot_tx)+ "_.txt";
-    
+    if (save_delays) {
+        
+    }
     cout << "Frame Asynchronous Coded Slotted ALOHA simulation \n";
     cout << "n = "<< n<<"\n";
     cout<< "d= "<< d<<"\n";
@@ -72,21 +81,29 @@ int main () {
     // Start timer
     clock_t begin = clock();
    
+    
+    
     g_index=g_len-1;
     while(g_index>=0)
     {
+        
+        evaluate_load(g[g_index], n, n_rx, d, Lambda, q, iter_max, how_often_to_decode, boundary_effect, first_slot_tx, num_packets_to_sim, num_PL_to_sim, PLR, g_index, maximum_delay, delays, save_delays);
+        
+        /*
         if(g_index+1<num_threads){
             num_threads=g_index+1;
         }
         
         for(int i=0; i<num_threads;i++){
-            t[i]=thread( evaluate_load,g[g_index], n, n_rx, d, Lambda, q, iter_max, how_often_to_decode, boundary_effect, first_slot_tx, num_packets_to_sim, num_PL_to_sim, &PLR, g_index, maximum_delay);
+            t[i]=thread( evaluate_load,g[g_index], n, n_rx, d, Lambda, q, iter_max, how_often_to_decode, boundary_effect, first_slot_tx, num_packets_to_sim, num_PL_to_sim, &PLR, g_index, maximum_delay, delays);
             g_index--;
         }
         
         for (int i=0; i<num_threads; i++) {
             t[i].join();
         }
+         */
+        g_index--;
     }
     
     
@@ -131,8 +148,24 @@ int main () {
     
     output_file<< endl;output_file<< endl; output_file<< endl; output_file<< endl;
     output_file<<"Load \t PLR"<< endl;
-    for (int i=g_len-1; i>=0; i--) {
+    for (int i=g_len-1; i>=0; i--)
+    {
         output_file << g[i]<<"\t"<<PLR[i]<<"\\\\"<<endl;
+    }
+    output_file << endl;
+    output_file << endl;
+    output_file <<"Delay PDF (one row per load and it is not normalized)" <<endl;
+    
+    if (save_delays)
+    {
+        for (int i=0; i<g_len; i++)
+        {
+            for (int ii=0; ii < maximum_delay; ii++)
+            {
+                output_file<< delays[i][ii]<<" ";
+            }
+            output_file <<"\\\\"<<endl;
+        }
     }
     output_file.close();
     clock_t end =clock();
@@ -143,11 +176,9 @@ int main () {
     return 0;
 }
 
-
-
 // This function is used to evaluate the performance for the given parameters (loop is usually over g, the system load).
-void evaluate_load(double g, int n, int n_rx,int d[], double Lambda[], int q, int iter_max, int how_often_to_decode, bool boundary_effect, bool first_slot_tx, int num_packets_to_sim, int num_PL_to_sim, vector<double> *PLR,int index,int maximum_delay)
-{
+    void evaluate_load(double g, int n, int n_rx,int d[],  double Lambda[],  int q,  int iter_max,  int how_often_to_decode,  bool boundary_effect,  bool first_slot_tx,  int num_packets_to_sim, int num_PL_to_sim, vector<double> &PLR,int index, int maximum_delay, vector<vector<int>> &delays, bool save_delays)
+    {
     int samp;
     unsigned long int time_step=0;
     Arrivals poiss= Arrivals(g);
@@ -216,6 +247,7 @@ void evaluate_load(double g, int n, int n_rx,int d[], double Lambda[], int q, in
             VN.push_back(temp_VN);
         }
         dec.decode(&CN, &VN, time_step);
+        
         // Counting packets: different method depending on the time_initial variable
         dec.count_packets(&VN, time_step, boundary_effect);
         packets_sent=dec.getSentPackets();
@@ -223,10 +255,13 @@ void evaluate_load(double g, int n, int n_rx,int d[], double Lambda[], int q, in
     }
     VN.clear();
     CN.clear();
-    PLR->at(index)=(double)packets_lost / (double)packets_sent;
+    if (save_delays)
+    {
+        delays.at(index) = dec.getDelays();
+    }
+    PLR.at(index)=(double)packets_lost / (double)packets_sent;
     streamsize normal_prec=cout.precision();
     cout<<setprecision(3)<<fixed;
-    cout<< g << " \t \t "<< setw((int)log10(num_packets_to_sim)+1)<< packets_sent << " \t \t" <<setw((int)log10(num_PL_to_sim)+1) << packets_lost << " \t \t "<< scientific<<PLR->at(index)<<endl;
+    cout<< g << " \t \t "<< setw((int)log10(num_packets_to_sim)+1)<< packets_sent << " \t \t" <<setw((int)log10(num_PL_to_sim)+1) << packets_lost << " \t \t "<< scientific<<PLR.at(index)<<endl;
     cout<<setprecision(normal_prec);
 }
-
