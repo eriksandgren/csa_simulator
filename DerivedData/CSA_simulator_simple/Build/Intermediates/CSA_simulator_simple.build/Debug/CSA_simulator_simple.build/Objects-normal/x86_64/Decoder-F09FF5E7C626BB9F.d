60c2ede5c205390dcae2d5090c7425437f605995
Erik Sandgren <eriksandgren@Eriks-MacBook-Pro.local>
2016-07-23 09:21:34 +0200

    Cleaning up

--- DerivedData/CSA_simulator_simple/Build/Intermediates/CSA_simulator_simple.build/Debug/CSA_simulator_simple.build/Objects-normal/x86_64/Decoder-F09FF5E7C626BB9F.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/eriksandgren/Dropbox/CSA_Simulator/CSA_simulator_simple/Decoder.cpp \
-  /Users/eriksandgren/Dropbox/CSA_Simulator/CSA_simulator_simple/Decoder.hpp \
-  /Users/eriksandgren/Dropbox/CSA_Simulator/CSA_simulator_simple/Node.hpp
--- DerivedData/CSA_simulator_simple/Build/Intermediates/CSA_simulator_simple.build/Release/CSA_simulator_simple.build/Objects-normal/x86_64/main.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/eriksandgren/Dropbox/CSA_Simulator/CSA_simulator_simple/main.cpp \
-  /Users/eriksandgren/Dropbox/CSA_Simulator/CSA_simulator_simple/Encoder.hpp \
-  /Users/eriksandgren/Dropbox/CSA_Simulator/CSA_simulator_simple/Node.hpp \
-  /Users/eriksandgren/Dropbox/CSA_Simulator/CSA_simulator_simple/Arrivals.hpp \
-  /Users/eriksandgren/Dropbox/CSA_Simulator/CSA_simulator_simple/Decoder.hpp
